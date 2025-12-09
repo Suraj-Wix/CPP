ed1cfc09fee586cc245d1903f1065b7ce376ccb5
Suraj-Wix <surajsharma87899@gmail.com>
2025-12-10 01:35:23 +0530

    initial commit

---
+++ WEEK-2-Assignments/main4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+for(int i=0;i<2*n-1;i++){
+    int cond=0;
+    if(i<n){//growing phase of Diamond
+        cond=i;
+    }
+    else{//shrinking phase of diamond
+        cond=n-(i%n)-2;
+
+    }
+    for(int j=0;j<=cond;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+}
