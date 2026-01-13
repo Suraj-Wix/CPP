523c0772863f614df7393ebe92b008320ffd4f3e
Suraj-Wix <surajsharma87899@gmail.com>
2026-01-13 12:49:07 +0530

    initial commit

---
+++ ArrayVectorSTL/main21.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// //visited method
+// void findMissing (int*a, int n){
+// for(int i=0;i<n;i++){
+//     int index=abs(a[i]);
+//     if(a[index-1]>0){
+//       a[index-1]*=-1;
+//     }
+
+// }
+// // for(int i=0;i<n;i++){
+// //     cout<<a[i]<<" ";
+// // }
+
+//   // all positive indexes are missing.
+//   for(int i=0;i<n;i++){
+//     if(a[i]>0)
+//     cout<<i+1<<" ";
+//   }
+
+// }
+
+// int main(){
+
+//     int n;
+//     int a[]={1,3,5,3,4};
+//     n=sizeof(a)/sizeof(int);
+//     // int size=5;
+//     findMissing(a,n);
+
+//     return 0;
+// }
+
+// method 2 sorting+swapping method
+void findMissing(int *a, int n)
+{
+    int i = 0;
+    while (i < n)
+    {
+       int index = a[i] - 1;
+        if (a[i] != a[index])
+        {
+            swap(a[i], a[index]);
+        }
+
+        else
+        {
+            i++;
+        }
+    }
+    // for(int i=0;i<n;i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
+    for(int i=0;i<n;i++){
+        if(a[i]!=i+1){
+            cout<<i+1<<" ";
+        }
+    }
+    cout<<endl;
+
+}
+
+int main()
+{
+    int n;
+    int a[] = {1, 3, 5, 3, 4};
+    n = sizeof(a) / sizeof(int);
+    // int size = 5;
+    findMissing(a, n);
+    return 0;
+}
